5fb652889b419262f13b05691c488e3db74cca73
Rick Koloski <rkoloski@tnsolutionsinc.com>
2012-06-15 12:04:46 -0400

    basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompassAeDocSite::Application.config.secret_token = 'd16ed1cf8005c2a6112194a6c3df867be72cfcf8c93cf8e358d427846bb9a03fdab59f0e59c524c22de6372cc24d4c7b4472cb4d383a503d6d461cdd6795b840'
